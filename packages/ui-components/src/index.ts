3b99b0fc6407ca8dc4caa5f2f5a0883e6528982d
Kien Nguyen <kien@xendit.co>
2026-03-07 23:33:13 +0700

    feat(ui-components): add accordion exports to barrel index

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -8,3 +8,10 @@ export type {
   ButtonStatus,
 } from "./components/ui-button.js";
 export { UiButtonGroup } from "./components/ui-button-group.js";
+export { UiAccordionItem } from "./components/ui-accordion-item.js";
+export type {
+  AccordionSize,
+  AccordionEmphasis,
+  AccordionStatus,
+} from "./components/ui-accordion-item.js";
+export { UiAccordionGroup } from "./components/ui-accordion-group.js";
